04acca3bff3dc8f700f9fa59338e5cc6fdea80de
mADKiNG-07 <jordanmhizcleo@gmail.com>
2022-10-25 10:17:26 +0100

    post route fixed

--- models/post.js
+++ models/post.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const postSchema = Schema({
     imgUrl: {
         type: String,
-        required: true
     },
     cryptoPair: {
         type: String,
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,75 +4,54 @@ const mAdmin = require('../middleware/mAdmin');
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
-const store = require('./../middleware/multer');
-const fileUpload = require('express-fileupload');
+const multer = require("multer");
 
 router.use(function (req, res, next) {
     res.header('Content-Type', 'application/json');
     next();
 });
 
-router.use(fileUpload());
+// set storage
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./public/img");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    },
+});
 
-router.post('/add-post', store.array('images', 12), (req, res) => {
-    const post = new Post(_.pick(req.body, "imgUrl", "cryptoPair", "typeOfTrade", "tradingPref", "desc", "rec", "outlook"));
-    post.imgUrl = req.file.filename;
+var upload = multer({
+    storage: Storage,
+}).single("file"); //Field name and max count
 
-    if (req.imgUrl === null) {
-        return res.status(400).json({ msg: 'No file uploaded' });
-    }
+router.post('/add-post', (req, res) => {
 
-    const file = req.imgUrl.file;
-
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    upload(req, res, function (err) {
         if (err) {
-            console.error(err);
-            return res.status(500).send(err);
-        }
+            console.log(err);
+            return res.end("Something went wrong");
+        } else {
+            const post = new Post(_.pick(req.body, "imgUrl", "cryptoPair", "typeOfTrade", "tradingPref", "desc", "rec", "outlook"));
 
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+            console.log(req.file.path);
+            var imageName = req.file.filename;
+
+            post.imgUrl = imageName;
+
+            post.save()
+                .then((result) => {
+                    res.send(JSON.stringify(result, null, 3) + "\n")
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
     });
 
-    post.save()
-        .then((result) => {
-            res.send(JSON.stringify(result, null, 3) + "\n")
-        })
-        .catch((err) => {
-            console.log(err)
-        })
 });
 
-router.put('/update-blog/:id', [mAuth, mAdmin], (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndUpdate(id,
-        {
-            title: req.body.title,
-            body: req.body.body,
-            imgUrl: req.body.imgUrl,
-            timeFrame: req.body.timeFrame
-        },
-        { new: true })
-        .then((result) => {
-            res.send(JSON.stringify(result, null, 3) + "\n")
-        })
-        .catch((err) => {
-            console.log(err)
-        });
-
-});
-
-router.delete('/delete-post/:id', [mAuth, mAdmin], (req, res) => {
-    const id = req.params.id;
-    Post.findByIdAndDelete(id)
-        .then((result) => {
-            res.send(JSON.stringify(result, null, 3) + "\n")
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-});
-
-router.get('/all-posts', mAuth, (req, res) => {
+router.get('/all-posts', (req, res) => {
     Post.find()
         .then((result) => {
             res.send(JSON.stringify(result, null, 3) + "\n")
@@ -104,6 +83,36 @@ router.get('/all-posts/time-frame/:timeFrame', mAuth, (req, res) => {
         })
 });
 
+
+router.put('/update-blog/:id', [mAuth, mAdmin], (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndUpdate(id,
+        {
+            title: req.body.title,
+            body: req.body.body,
+            imgUrl: req.body.imgUrl,
+            timeFrame: req.body.timeFrame
+        },
+        { new: true })
+        .then((result) => {
+            res.send(JSON.stringify(result, null, 3) + "\n")
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+
+});
+
+router.delete('/delete-post/:id', [mAuth, mAdmin], (req, res) => {
+    const id = req.params.id;
+    Post.findByIdAndDelete(id)
+        .then((result) => {
+            res.send(JSON.stringify(result, null, 3) + "\n")
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+});
 
 
 module.exports = router;
