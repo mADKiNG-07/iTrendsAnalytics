51dc48fb5edb270d2be6b4bc522ddd9c3e8567cb
mADKiNG-07 <jordanmhizcleo@gmail.com>
2022-12-26 12:39:51 +0100

    fixes

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ require('./startup/db')();
 
 // Add headers
 app.use(function (req, res, next) {
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -27,10 +31,6 @@ app.use(function (req, res, next) {
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Accept, Authortization');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
